6e03c0ed1a6366f67e85c5094df8b07020ec7ed1
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2023-11-29 12:57:40 +0100

    Update app.js
    Se incluye cors en index.js
    
    1. Se importa:
    const cors = require('cors');
    
    2. Se indican urls de acceso:
    app.use(
      cors({
        origin: "*",    
        //origin: "http://localhost:3001/",
        credentials: true,
      })
    );
    
    
    Permite llamar directamente desde FRONT al BACK usando endpoints diferentes: 
    - Front: http://localhost:3000
    - Back:  http://localhost:5000

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Importo servidor. 
 const express = require('express');
 
+//! Permite llamar al back desde el FRONT
+const cors = require('cors');
+
 // Importo conexión a BBDD.
 const { connect } = require('./src/bd');
 
@@ -11,6 +14,15 @@ const PORT = 5001;
 const app = express();
 
 connect();
+
+//! Permite llamar al back desde el FRONT usando un endpoint diferente. localhost:3000/5000. 
+app.use(
+  cors({
+    origin: "*",    
+    //origin: "http://localhost:3001/",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
